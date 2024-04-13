1965d314080e8e9291ca123a85d99859aca83d30
aryan-kharva-04 <156562271+aryan-kharva-04@users.noreply.github.com>
2024-04-13 15:07:05 -0400

    fixes

---
+++ itis3135/client project/exhibitions.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.carousel-slide').slick({
+    slidesToShow: 1, // Number of slides shown at once
+    slidesToScroll: 1, // Number of slides scrolled at a time
+    autoplay: true,
+    autoplaySpeed: 3000, // Interval time between slides (in milliseconds)
+    prevArrow: '.carousel-prev',
+    nextArrow: '.carousel-next',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
